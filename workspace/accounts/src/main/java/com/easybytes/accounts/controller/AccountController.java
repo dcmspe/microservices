300d6f24b464005a392707520fd7ed4689fbba5b
danilo-santos-hexa <danilo.santos@hexa-consulting.pt>
2025-11-03 11:08:23 +0000

    Improving the api documentation

--- workspace/accounts/src/main/java/com/easybytes/accounts/controller/AccountController.java
+++ workspace/accounts/src/main/java/com/easybytes/accounts/controller/AccountController.java
@@ -2,8 +2,14 @@ package com.easybytes.accounts.controller;
 
 import com.easybytes.accounts.constants.AccountsConstants;
 import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.dto.ErrorResponseDto;
 import com.easybytes.accounts.dto.ResponseDto;
 import com.easybytes.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -26,6 +32,12 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
 
     private IAccountsService iAccountsService;
+    @Operation(summary = "Create Account REST API",
+    description = "REST API to create new Customer and Account inside EasyBank")
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+    )
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
@@ -35,12 +47,30 @@ public class AccountController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(summary = "Fetch Account REST API",
+            description = "REST API to fetch new Customer and Account details based on a mobile number")
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "^|[0-9]{10}", message = "Mobile Number must be 10 digits") String mobileNumber){
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.ok(customerDto);
     }
 
+    @Operation(summary = "Update Account REST API",
+            description = "REST API to update Customer and Account details based on a account number")
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "200",
+                description = "HTTP Status OK"
+            ),
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server ERROR"
+        )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto){
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
@@ -52,6 +82,23 @@ public class AccountController {
         }
     }
 
+    @Operation(summary = "Delete Account & Customer Details REST API",
+            description = "REST API to update Customer and Account details based on a account number")
+    @ApiResponses({
+            @ApiResponse(
+                responseCode = "200",
+                description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server ERROR",
+                content = @Content(
+                    schema = @Schema(
+                        implementation = ErrorResponseDto.class
+                    )
+                )
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam @Pattern(regexp = "^|[0-9]{10}", message = "Mobile Number must be 10 digits") Long mobileNumber){
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber.toString());
--- workspace/accounts/src/main/java/com/easybytes/accounts/dto/ResponseDto.java
+++ workspace/accounts/src/main/java/com/easybytes/accounts/dto/ResponseDto.java
@@ -1,15 +1,22 @@
 package com.easybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(
+    name="Response",
+    description="Schema to hold successful response information"
+)
 @Data
 @AllArgsConstructor
 public class ResponseDto {
 
+    @Schema(description = "State code in the response", example = "200")
     private String statusCode;
 
+    @Schema(description = "Status message in the response", example = "Request processed successfully")
     private String statusMessage;
 
 }
